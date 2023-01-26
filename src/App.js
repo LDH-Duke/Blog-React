af4c389ea4929657dd91f74253e6dbd87533836b
LDH-Duke <dukeldh1128@gmail.com>
2023-01-26 18:33:05 +0900

    리액트 연습 세팅

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let [subject,subjectUpdate] = useState(['동래 맛집 추천','경성대부경대 맛집 추천','서면 맛집 추천']);
+  let [goodCount, addCount] = useState([0,0,0]); 
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header-nav">
+        <div className='nav-logo'>
+          <span>Blog</span>
+        </div>
+        <button onClick={()=>{}}>정렬</button>
+      </div>
+      <div className='list'>
+          <ul>
+            <li>
+              <h4>{subject[0]}<span onClick={()=>{goodCount = [...goodCount]; goodCount[0] += 1; addCount(goodCount)}}>👍</span>{goodCount[0]}</h4>
+              <p>2월17일 발행</p>
+              <button onClick={()=>{let copy=[...subject]; copy[0] = '전포 맛집 추천'; subjectUpdate(copy)}}>변경</button>
+            </li>
+            <li><h4>{subject[1]}<span onClick={()=>{goodCount = [...goodCount]; goodCount[1] += 1; addCount(goodCount)}}>👍</span>{goodCount[1]}</h4><p>2월18일 발행</p></li>
+            <li><h4>{subject[2]}<span onClick={()=>{goodCount = [...goodCount]; goodCount[2] += 1; addCount(goodCount)}}>👍</span>{goodCount[2]}</h4><p>2월19일 발행</p></li>
+          </ul>
+        </div>
     </div>
   );
 }
---
+++ src/component/header_nav.js
@@ -0,0 +1,21 @@
+export default function header_nav(){
+    return (
+        <div>
+            <div className="nav">
+                <div className="nav-item-group">
+                    <div className="nav-logo">
+                        <span className="logo">Just-Do BookStore</span>
+                    </div>
+                    <div className="nav-list">
+                        <ul>
+                            <li>국내 도서</li>
+                            <li>외국 도서</li>
+                            <li>중고 도서</li>
+                            <li>eBook</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
